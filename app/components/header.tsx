7d290195cba1e491b75b8f5429a8dbefc7f65ed8
Jayden Burrows <jaydenborrows@gmail.com>
2026-03-12 13:45:18 -0400

    modal addition: 1/4

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,5 +1,6 @@
-"use client"
+"use client" 
 import { useState } from "react";
+import { motion, AnimatePresence } from "motion/react";
 import Modal from "./modal";
 
 const baseImgPath = process.env.NODE_ENV === 'production'
@@ -38,16 +39,28 @@ function Subheading({ about }: SubheadingProps) {
     if (about) {
         return(
             <div>
-                <p className="address">Jade Palace, Valley of Peace </p>
-                <p className="tagline"> Dragon Warrior, Savior of China, and Part-time Noodle Maker
+                <p className="address" key="address">Jade Palace, Valley of Peace </p>
+                <p className="tagline" key="tagline"> Dragon Warrior, Savior of China, and Part-time Noodle Maker
                 </p>
-                <button className="modalOpenButton"
+                <button 
+                    className="modalOpenButton"
                     onClick={() => setModalOpen(prevState => !prevState)}
+                    key="modalOpenButton"
                 > 
                     Resume 
                 </button>
-                {modalOpen && <Modal handleClose={close}/>}
-                <hr />
+                <AnimatePresence
+                // disable initial animations are children
+                // that are present when the component is first rendered
+                initial={false}
+                // Only render one component at a time.
+                // The exiting component will finish its exit
+                // animation before the entering component is rendered
+                onExitComplete={() => null}
+                >   
+                    {modalOpen && <Modal handleClose={close} key="modal"/>}
+                    <hr key="hr"/>
+                </AnimatePresence>
             </div>
         );
     } else {
